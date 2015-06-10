7815e34a65b77b9559139eae80404d444c809d6b
Miguel Angel Gutiérrez <magutierrezsistemas@gmail.com>
2015-06-10 13:34:01 +0200

    Create webdata.py

---
+++ webdata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import urllib2
+
+response=urllib2.urlopen('http://www.unapaginacualquiera.com') #poner aquí el nombre de la página que nos interesa
+cod=response.read()
+
+#Obtenemos el codigo de la pagina
+#Ahora debemos almacenarlo en un archivo de texto
+
+#abrimos un flujo de escritura
+a=open("archivoejemplo.txt","w") #abrimos/creamos el archivo con permiso de escritura
+a.write(cod) #escribimos el codigo obtenido de la web
+a.close() #cerramos el flujo de escritura
